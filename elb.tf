24d429e711fa1155a4086865a44ec03e373cbc7e
sheetalpattanaik <125354824+sheetalpattanaik@users.noreply.github.com>
2023-03-21 13:08:44 +0530

    elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
-  availability_zones = ["ap-south-1a", "ap-south-1b"]
+  name               = "sheetal-terraform-elb"
+  availability_zones = ["us-east-1a", "us-east-1b"]
 
   listener {
     instance_port     = 80
@@ -21,6 +21,6 @@ resource "aws_elb" "bar" {
   cross_zone_load_balancing = true
   idle_timeout              = 400
   tags = {
-    Name = "raham-tf-elb"
+    Name = "sheetal-tf-elb"
   }
 }
